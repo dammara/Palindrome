95651c9d8e4c0e81eda68f97382b5e319bff5f17
dammara <h3246531>
2020-04-01 13:12:14 -0400

    4/1/2020 1:11pm - Program runs, except the white spaces, despite my atempts to remove them, still show up in palindromes like "race car", and as a result, the program doesnt recognize it as a palindrome.

---
+++ onlineexample.py
@@ -0,0 +1,15 @@
+# THis is an online example program I'm looking at to see how this thing runs. Swear I'm copying it.
+
+def is_palindrome(s):
+    if len(s) < 1:
+        return True
+    else:
+        if s[0] == s[-1]:
+            return is_palindrome(s[1:-1])
+        else:
+            return False
+a=str(input("Enter string:"))
+if(is_palindrome(a)==True):
+    print("String is a palindrome!")
+else:
+    print("String isn't a palindrome!")
